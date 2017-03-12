7e36a7d8be2788438038eb45e57e39c6c85b54ed
LWWenz <leongww7@gmail.com>
2017-03-12 20:45:18 +0800

    Rebranch priority from master Add priorityList to store the lists of priority tasks Add methods to change the priority of the task

--- src/main/java/typetask/model/task/Priority.java
+++ src/main/java/typetask/model/task/Priority.java
@@ -1,31 +1,60 @@
 package typetask.model.task;
 
+import java.util.Vector;
+
 public class Priority {
 
     private boolean priority;
 
     public static final String MESSAGE_PRIORITY_CONSTRAINTS =
             "Priority should be either in true or false.";
+    
+    //priorityList stores the list of priority tasks
+    private static Vector<Task> priorityList;
+    private Task task;
 
-    public Priority () {
+    /**
+     * Tasks added without priority will be false by default.
+     * @param task
+     */
+    public Priority (Task task) {
         this.priority = false;
+        this.task = task;
     }
 
-    public Priority(boolean priority) {
-        this.priority = priority;
+    public Priority(boolean priority, Task task) {
+        this.task = task;
+        addPriority();
     }
 
-    public Priority(String priority) {
+    public Priority(String priority, Task task) {
         String amended = priority.substring(2).trim();
+
+        this.task = task;
+        
         if (amended.toLowerCase().compareTo("true") == 0) {
-            this.priority = true;
+            addPriority();
         } else {
             this.priority = false;
         }
     }
 
+    /**
+     * Returns the priority of the task
+     * @return the boolean priority of the priority
+     */
     public boolean getPriority() {
-        return priority;
+        return this.priority;
+    }
+    
+    public void removePriority() {
+      priorityList.remove(this.task);
+      this.priority = false;
+    }
+    
+    public void addPriority() {
+      priorityList.addElement(task);
+      this.priority = true;
     }
 
 
