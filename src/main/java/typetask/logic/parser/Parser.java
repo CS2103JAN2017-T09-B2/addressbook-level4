afa4d412a103a63ecbafb85f8e9420f2790b4fc5
Dana Baghabrah <d.baghabra@gmail.com>
2017-03-20 14:18:08 +0800

    Fix indent;

--- src/main/java/typetask/logic/parser/Parser.java
+++ src/main/java/typetask/logic/parser/Parser.java
@@ -37,62 +37,60 @@ public class Parser {
 	 * @return the command based on the user input
 	 */
     public Command parseCommand(String userInput) {
-		final Matcher matcher = BASIC_COMMAND_FORMAT.matcher(userInput.trim());
-		if (!matcher.matches()) {
-			return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT, HelpCommand.MESSAGE_USAGE));
-		}
+        final Matcher matcher = BASIC_COMMAND_FORMAT.matcher(userInput.trim());
+        if (!matcher.matches()) {
+            return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT, HelpCommand.MESSAGE_USAGE));
+        }
 
-		final String commandWord = matcher.group("commandWord");
-		final String arguments = matcher.group("arguments");
-		switch (commandWord) {
+        final String commandWord = matcher.group("commandWord");
+        final String arguments = matcher.group("arguments");
+        switch (commandWord) {
+            case AddCommand.COMMAND_WORD:
+                return new AddCommandParser().parse(arguments);
+            case AddCommand.COMMAND_WORD2:
+                return new AddCommandParser().parse(arguments);
+            case AddCommand.COMMAND_WORD3:
+                return new AddCommandParser().parse(arguments);
 
-		case AddCommand.COMMAND_WORD:
-			return new AddCommandParser().parse(arguments);
-		case AddCommand.COMMAND_WORD2:
-			return new AddCommandParser().parse(arguments);
-		case AddCommand.COMMAND_WORD3:
-			return new AddCommandParser().parse(arguments);
+            case EditCommand.COMMAND_WORD:
+                return new EditCommandParser().parse(arguments);
+                
+            case SelectCommand.COMMAND_WORD:
+                return new SelectCommandParser().parse(arguments);
 
-		case EditCommand.COMMAND_WORD:
-			return new EditCommandParser().parse(arguments);
+            case DeleteCommand.COMMAND_WORD:
+                return new DeleteCommandParser().parse(arguments);
+            case DeleteCommand.COMMAND_WORD2:
+                return new DeleteCommandParser().parse(arguments);
+            case DeleteCommand.COMMAND_WORD3:
+                return new DeleteCommandParser().parse(arguments);
 
-		case SelectCommand.COMMAND_WORD:
-			return new SelectCommandParser().parse(arguments);
+            case DoneCommand.COMMAND_WORD:
+                return new DoneCommandParser().parse(arguments);
+            case DoneCommand.COMMAND_WORD2:
+                return new DoneCommandParser().parse(arguments);
 
-		case DeleteCommand.COMMAND_WORD:
-			return new DeleteCommandParser().parse(arguments);
-		case DeleteCommand.COMMAND_WORD2:
-			return new DeleteCommandParser().parse(arguments);
-		case DeleteCommand.COMMAND_WORD3:
-			return new DeleteCommandParser().parse(arguments);
+            case ClearCommand.COMMAND_WORD:
+                return new ClearCommand();
 
-		case DoneCommand.COMMAND_WORD:
-			return new DoneCommandParser().parse(arguments);
-		case DoneCommand.COMMAND_WORD2:
-			return new DoneCommandParser().parse(arguments);
+            case FindCommand.COMMAND_WORD:
+                return new FindCommandParser().parse(arguments);
+            case FindCommand.COMMAND_WORD2:
+                return new FindCommandParser().parse(arguments);
+            case FindCommand.COMMAND_WORD3:
+                return new FindCommandParser().parse(arguments);
 
-		case ClearCommand.COMMAND_WORD:
-			return new ClearCommand();
+            case ListCommand.COMMAND_WORD:
+                return new ListCommand();
 
-		case FindCommand.COMMAND_WORD:
-			return new FindCommandParser().parse(arguments);
-		case FindCommand.COMMAND_WORD2:
-			return new FindCommandParser().parse(arguments);
-		case FindCommand.COMMAND_WORD3:
-			return new FindCommandParser().parse(arguments);
+            case ExitCommand.COMMAND_WORD:
+                return new ExitCommand();
 
-		case ListCommand.COMMAND_WORD:
-			return new ListCommand();
+            case HelpCommand.COMMAND_WORD:
+                return new HelpCommand();
 
-		case ExitCommand.COMMAND_WORD:
-			return new ExitCommand();
-
-		case HelpCommand.COMMAND_WORD:
-			return new HelpCommand();
-
-		default:
-			return new IncorrectCommand(MESSAGE_UNKNOWN_COMMAND);
-		}
+            default:
+                return new IncorrectCommand(MESSAGE_UNKNOWN_COMMAND);
+         }
 	}
-
 }
