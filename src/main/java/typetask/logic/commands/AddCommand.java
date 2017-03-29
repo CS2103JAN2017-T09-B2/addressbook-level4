f468f4d7a6ffa44304f5d69770a2c1846e3537f6
Dana Baghabrah <d.baghabra@gmail.com>
2017-03-30 02:07:46 +0800

    fix;

--- src/main/java/typetask/logic/commands/AddCommand.java
+++ src/main/java/typetask/logic/commands/AddCommand.java
@@ -51,7 +51,7 @@ public class AddCommand extends Command {
 
     public AddCommand(String name, String date, String endDate, String priority)
             throws IllegalValueException {
-        this.toAdd = new Task(new Name(name), new DueDate("date"), new DueDate("endDate"), 
+        this.toAdd = new Task(new Name(name), new DueDate("date"), new DueDate("endDate"),
                 false, new Priority(priority));
     }
 
